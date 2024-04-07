575c74036cfd88c27842d8acfe72b53d9aa8e69f
Chris Thibault <rchristhibault@gmail.com>
2024-04-06 21:25:35 -0400

    solve length of last word

---
+++ leetcode/lengthoflastword.js
@@ -0,0 +1,13 @@
+var lengthOfLastWord = function(s) {
+    s = s.trim();
+
+    let length = 0;
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] === ' ') {
+            break;
+        }
+        length++;
+    }
+
+    return length;
+};

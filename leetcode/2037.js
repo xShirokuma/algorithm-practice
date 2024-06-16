d8d6d52b82f0d0c518952d60371e2cf24ca05469
Chris Thibault <rchristhibault@gmail.com>
2024-06-16 00:43:06 -0400

    solve leetcode 2037

---
+++ leetcode/2037.js
@@ -0,0 +1,11 @@
+function minMovesToSeat(seats, students) {
+    seats.sort((a, b) => a - b);
+    students.sort((a, b) => a - b);
+
+    let totalMoves = 0;
+    for (let i = 0; i < seats.length; i++) {
+        totalMoves += Math.abs(seats[i] - students[i]);
+    }
+
+    return totalMoves;
+}

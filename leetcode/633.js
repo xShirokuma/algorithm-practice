c8f4df4d34475ab12ac826dbb358d3273a0924b6
Chris Thibault <rchristhibault@gmail.com>
2024-06-23 00:34:44 -0400

    solve leetcode 633

---
+++ leetcode/633.js
@@ -0,0 +1,18 @@
+var judgeSquareSum = function(c) {
+    let left = 0;
+    let right = Math.floor(Math.sqrt(c));
+
+    while (left <= right) {
+        let sumOfSquares = left * left + right * right;
+
+        if (sumOfSquares === c) {
+            return true;
+        } else if (sumOfSquares < c) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return false;
+};

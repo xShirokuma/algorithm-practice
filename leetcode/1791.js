2919d27ef4313112fdcc10d4ec1af576a7b48776
Chris Thibault <rchristhibault@gmail.com>
2024-06-30 00:13:22 -0400

    solve leetcode 1791

---
+++ leetcode/1791.js
@@ -0,0 +1,10 @@
+var findCenter = function(edges) {
+    let [u1, v1] = edges[0];
+    let [u2, v2] = edges[1];
+
+    if (u1 === u2 || u1 === v2) {
+        return u1;
+    } else {
+        return v1;
+    }
+};

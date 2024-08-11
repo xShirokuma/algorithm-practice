87a9b37d41f2fcdafeb61e9bdab8970de71245aa
Chris Thibault <rchristhibault@gmail.com>
2024-08-11 00:35:46 -0400

    solve leetcode 1508

---
+++ leetcode/1508.js
@@ -0,0 +1,21 @@
+function rangeSum(nums, n, left, right) {
+    const MOD = 1e9 + 7;
+    let subarraySums = [];
+
+    for (let i = 0; i < n; i++) {
+        let sum = 0;
+        for (let j = i; j < n; j++) {
+            sum += nums[j];
+            subarraySums.push(sum);
+        }
+    }
+
+    subarraySums.sort((a, b) => a - b);
+
+    let result = 0;
+    for (let i = left - 1; i < right; i++) {
+        result = (result + subarraySums[i]) % MOD;
+    }
+
+    return result;
+}

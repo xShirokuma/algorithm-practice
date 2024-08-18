fb7f0eebef894a0cba6605c0e027d080ab80ced9
Chris Thibault <rchristhibault@gmail.com>
2024-08-18 00:32:56 -0400

    solve leetcode 624

---
+++ leetcode/624.js
@@ -0,0 +1,17 @@
+var maxDistance = function(arrays) {
+    let minVal = arrays[0][0];
+    let maxVal = arrays[0][arrays[0].length - 1];
+    let maxDist = 0;
+
+    for (let i = 1; i < arrays.length; i++) {
+        let currentMin = arrays[i][0];
+        let currentMax = arrays[i][arrays[i].length - 1];
+
+        maxDist = Math.max(maxDist, Math.abs(currentMax - minVal), Math.abs(maxVal - currentMin));
+
+        minVal = Math.min(minVal, currentMin);
+        maxVal = Math.max(maxVal, currentMax);
+    }
+
+    return maxDist;
+};
